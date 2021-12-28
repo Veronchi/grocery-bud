630422cafbcb62d3c361bdd0cd7e2045c8b8b9ab
Veronchi <zhigunova1995@gmail.com>
2021-12-28 15:18:15 +0300

    implement form with store input value at the state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={(e) => handleSubmit(e)}>
+        <h3>grpcery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text' className='grocery'
+            placeholder='e.g. eggs'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>submiit</button>
+        </div>
+      </form>
+    </section>
+  )
 }
 
 export default App
