b14656f020546b4cf80454a6b57971a502ee3d86
Veronchi <zhigunova1995@gmail.com>
2022-01-02 20:16:39 +0300

    implement adding items of the list

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('');
+  // const [isSubmit, setIsSubmit] = useState(false);
+  const [listItems, setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setList([...listItems, inputValue]);
+    setInputValue('')
+  }
+
+  const handleInputState = (e) => {
+    e.preventDefault();
+    setInputValue(e.target.value)
   }
 
   return (
     <section className='section-center'>
-      <form className='grocery-form' onSubmit={(e) => handleSubmit(e)}>
-        <h3>grpcery bud</h3>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        <h3>grocery bud</h3>
         <div className='form-control'>
           <input
             type='text' className='grocery'
             placeholder='e.g. eggs'
             value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            onChange={handleInputState}
           />
           <button type='submit' className='submit-btn'>submiit</button>
         </div>
       </form>
+      <div className='grocery-container'>
+        {listItems.length ? <List items={listItems} /> : null}
+      </div>
     </section>
   )
 }
--- src/List.js
+++ src/List.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+
+const List = ({ items }) => {
+
+  return (
+    <ul className='grocery-list'>
+      {items.map((item, id) => {
+        return (
+          <li key={`item${id}`} className='grocery-item'>
+            <p className='title'>{item}</p>
+            <div className='btn-container'>
+              <button type='button' className='edit-btn'>
+                <FaEdit />
+              </button>
+              <button type='button' className='delete-btn'>
+                <FaTrash />
+              </button>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  )
 }
 
 export default List
