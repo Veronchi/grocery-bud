8d75ba27a6e25c1b309552718d6e2c0adc2bccaf
Veronchi <zhigunova1995@gmail.com>
2022-01-06 19:52:34 +0300

    implement clearing message of alert

--- src/Alert.js
+++ src/Alert.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const Alert = ({ status, messageText, clearAlert }) => {
-  let message = <p className={`alert alert-${status}`} >{messageText}</p>;
+  let message = <p className={`alert alert-${status}`}>{messageText}</p>;
 
-  return (
-    <React.Fragment>
-      {message}
-    </React.Fragment>
-  );
-}
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      clearAlert();
+    }, 3000);
 
-export default Alert
+    return () => clearTimeout(timer);
+  }, [clearAlert]);
+
+  return <React.Fragment>{message}</React.Fragment>;
+};
+
+export default Alert;
